51be12fa3edd3d9175d88a06608588140d58ceb5
fabiangro <118316299+fabiangro@users.noreply.github.com>
2023-01-23 18:20:58 +0100

    Coefficient form of polynomial

--- polynomials.py
+++ polynomials.py
@@ -1,5 +1,54 @@
 from numpy.polynomial import polynomial as P
 
+# 1
+
+def coefficient_form(f):
+    def coefficient(x):
+        if "x" not in x:
+            return int(x)
+        elif "^" not in x:
+            if x[0] != "x":
+                return int(x[:-1])
+            else:
+                return 1
+        else:
+            if x[0] != "x":
+                return int(x[:(x.index("x"))])
+            else:
+                return 1
+    def power(x):
+        if "x" not in x:
+            return 0
+        elif "^" not in x:
+            return 1
+        else:
+            return int(x[x.index("^") + 1:])
+
+    f = f.split()
+    f = [x for x in f if x[:2] != "0x"]
+    f0 = []
+    if "x" in f[0]:
+        f0.append(f[0])
+    for i in range(len(f)):
+        if f[i] == "+":
+            f0.append(f[i+1])
+        elif f[i] == "-":
+            if f[i] + f[i+1][0] == "-x":
+                f0.append("-1" + f[i+1])
+            else:
+                f0.append("-" + f[i+1])
+    if f0:
+        f = f0
+    powers = [power(x) for x in f]
+    coefficients = [coefficient(x) for x in f]
+    degree = powers[0]
+    wyn = [0]*(degree+1)
+    x = zip(powers,coefficients)
+
+    for i, j in x:
+        wyn[i] = j
+    return(wyn[::-1])
+
 #3
 
 def eval(f,x):
